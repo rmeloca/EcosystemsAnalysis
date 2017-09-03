2c540d3ce4543da9a8f94ee54f0d552aaa15f555
Rômulo Manciola Meloca <rmeloca@gmail.com>
2017-09-02 22:17:40 -0300

    cran fetch dependencies added and fixes

--- fetchDependencies.py
+++ fetchDependencies.py
@@ -2,6 +2,7 @@ import sys
 import os
 import requests
 import json
+from bs4 import BeautifulSoup
 from ecosystemDataManager.ecosystemDataManager import EcosystemDataManager
 
 def getContent(url):
@@ -79,6 +80,7 @@ def fetchRubygemsPackages():
     packages = packages.split("\n")
     packages.pop(0)
     packages.pop(0)
+    del packages[len(packages) - 1]
     for package in packages:
         split = package.split(" ")
         RUBYGEMS_PACKAGES_HAS_VERSIONS[split[0]] = split[1].split(",")
@@ -89,54 +91,111 @@ def fetchRubygems(package):
     registry = 'https://rubygems.org/api/v2/rubygems/'
     versions = RUBYGEMS_PACKAGES_HAS_VERSIONS[package.getName()]
     for metadataVersion in versions:
-        metadata = getJson(os.path.join(registry, package.getName(), "versions", metadataVersion + ".json"))
-        version = package.addVersion(metadataVersion)
         try:
-            version.setLicenses(metadata["licenses"])
+            metadata = getJson(os.path.join(registry, package.getName(), "versions", metadataVersion + ".json"))
+            version = package.addVersion(metadataVersion)
+            try:
+                version.setLicenses(metadata["licenses"])
+            except Exception as e:
+                print(package.getName() + "@" + metadataVersion, "no licenses")
+            try:
+                version.addLicense(metadata["license"])
+            except Exception as e:
+                print(package.getName() + "@" + metadataVersion, "no license")
+            version.setDatetime(metadata["created_at"])
+            version.setAuthors({metadata["authors"]: metadata["mailing_list_uri"]})
+            version.setDownloads(metadata["version_downloads"])
+            try:
+                for metadataDependency in metadata["dependencies"]["runtime"]:
+                    key = metadataDependency["name"]
+                    value = metadataDependency["requirements"]
+                    requirements = value
+                    split = value.replace("x", "0").split(" ")
+                    delimiter = split[0]
+                    value = split[1]
+                    dependencyPackage = ecosystemDataManager.addPackage(key)
+                    dependencyVersion = dependencyPackage.addVersion(value)
+                    dependency = version.addDependency(dependencyVersion)
+                    dependency.setDelimiter(delimiter)
+                    dependency.setRequirements(requirements)
+            except Exception as e:
+                print(package.getName() + "@" + metadataVersion, "no dependencies")
         except Exception as e:
-            print(package.getName() + "@" + metadataVersion, "no licenses")
-        try:
-            version.addLicense(metadata["license"])
-        except Exception as e:
-            print(package.getName() + "@" + metadataVersion, "no license")
-        version.setDatetime(metadata["created_at"])
-        version.setAuthors({metadata["authors"]: metadata["mailing_list_uri"]})
-        version.setDownloads(metadata["version_downloads"])
-        try:
-            for metadataDependency in metadata["dependencies"]["runtime"]:
-                key = metadataDependency["name"]
-                value = metadataDependency["requirements"]
-                requirements = value
-                split = value.replace("x", "0").split(" ")
-                delimiter = split[0]
-                value = split[1]
-                dependencyPackage = ecosystemDataManager.addPackage(key)
-                dependencyVersion = dependencyPackage.addVersion(value)
-                dependency = version.addDependency(dependencyVersion)
-                dependency.setDelimiter(delimiter)
-                dependency.setRequirements(requirements)
-        except Exception as e:
-            print(package.getName() + "@" + metadataVersion, "no dependencies")
+            print(package.getName() + "@" + metadataVersion, "VERSION FETCH FAIL")
+
+def fetchCran(package):
+    ecosystemDataManager = package.getEcosystemDataManager()
+    registry = "https://cran.r-project.org/web/packages/"
+    metadata = getContent(os.path.join(registry, package.getName()))
+    soup = BeautifulSoup(metadata, "lxml")
+    table = soup.find("table")
+    dependencies = []
+    for tr in table.findAll("tr"):
+        td = tr.findAll("td")
+        if (td[0].getText() == "Version:"):
+            metadataVersion = td[1].getText().replace("\n", "")
+        elif (td[0].getText() == "License:"):
+            licenses = td[1].getText().replace("\n", "").split(", ")
+        elif (td[0].getText() == "Depends:"):
+            dependencies += td[1].getText().replace("\n", "").split(", ")
+        elif (td[0].getText() == "Imports:"):
+            dependencies += td[1].getText().replace("\n", "").split(", ")
+        elif (td[0].getText() == "Published:"):
+            datetime = td[1].getText().replace("\n", "")
+        elif (td[0].getText() == "Maintainer:"):
+            split = td[1].getText().replace("\n", "").replace(">", "").split("<")
+            authors = {split[1].strip(): split[0].strip()}
+    version = package.addVersion(metadataVersion)
+    version.setLicenses(licenses)
+    version.setDatetime(datetime)
+    version.setAuthors(authors)
+    for metadataDependency in dependencies:
+        split = metadataDependency.replace("(", "").replace(")", "").split(" ")
+        value = None
+        delimiter = None
+        key = split[0]
+        if len(split) > 2:
+            delimiter = split[1]
+            value = split[2]
+        elif len(split) == 2:
+            value = split[1]
+        requirements = metadataDependency
+        dependencyPackage = ecosystemDataManager.addPackage(key)
+        dependencyVersion = dependencyPackage.addVersion(value)
+        dependency = version.addDependency(dependencyVersion)
+        dependency.setDelimiter(delimiter)
+        dependency.setRequirements(requirements)
 
 def fetch(ecossystem, package):
-    if ecossystem == "npm":
-        fetchNpm(package)
-    elif ecossystem == "rubygems":
-        fetchRubygems(package)
+    try:
+        if ecossystem == "npm":
+            fetchNpm(package)
+        elif ecossystem == "rubygems":
+            fetchRubygems(package)
+        elif ecossystem == "cran":
+            fetchCran(package)
+    except Exception as e:
+        print(package.getName(), "NO NETWORK CONNECTION. FETCH FAIL", e)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Usage:", sys.argv[0], "<ecossystem>")
+        print("Usage:", sys.argv[0], "<ecossystem> [<limit>]")
         sys.exit(1)
+    if len(sys.argv) == 3:
+        limit = int(sys.argv[2])
+    else:
+        limit = -1
     ecossystem = sys.argv[1]
     ecosystemDataManager = EcosystemDataManager(ecossystem)
     packages = ecosystemDataManager.getPackages()
-    limit = 0
     if ecossystem == "rubygems":
         fetchRubygemsPackages()
+    index = 0
+    size = len(packages)
     for package in packages:
+        print(index, "/", limit, "/", size)
         fetch(ecossystem, package)
-        limit += 1
-        if limit == 1000:
+        index += 1
+        if index == limit:
             break
     ecosystemDataManager.save()
