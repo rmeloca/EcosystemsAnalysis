758a78d6b271e52e6ad376145ed28dc2daad41a0
Rômulo Manciola Meloca <rmeloca@gmail.com>
2017-09-22 05:33:26 -0300

    merged boxplot.py from debug

--- boxplot.py
+++ boxplot.py
@@ -3,12 +3,26 @@ import plotly
 import plotly.graph_objs as go
 from ecosystemDataManager.ecosystemDataManager import EcosystemDataManager
 
-def plotBoxPlot(vector):
+def histogram(vector, name_histogram):
+    trace = go.Histogram(
+        name='Results',
+        x = vector,
+        xbins=dict(
+            start=1,
+            end=150,
+            size=0.5
+        )
+    )
+    data = [trace]
+    plotly.offline.plot(data, filename=name_histogram)
+    
+
+def plotBoxPlot(vector, name_boxplot):
     trace0 = go.Box(
         y=vector
     )
     data = [trace0]
-    plotly.offline.plot(data)
+    plotly.offline.plot(data, filename=name_boxplot)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -17,4 +31,5 @@ if __name__ == '__main__':
     ecossystem = sys.argv[1]
     ecosystemDataManager = EcosystemDataManager(ecossystem)
     packageSizeDistribution = ecosystemDataManager.getPackageSizeDistribution()
-    plotBoxPlot(packageSizeDistribution)
+    plotBoxPlot(packageSizeDistribution, 'boxplot_packageSizeDistribution')
+    histogram(packageSizeDistribution, 'histogram_packageSizeDistribution')
