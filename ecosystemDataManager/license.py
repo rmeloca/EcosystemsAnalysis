a4ec24158122d4c2319c722eaa19b535b0f066f8
marcocmm <marco.cmm@gmail.com>
2017-11-30 14:29:10 -0200

    Documentation license.py and ocurrence.py

--- ecosystemDataManager/license.py
+++ ecosystemDataManager/license.py
@@ -1,35 +1,56 @@
 from .group import Group
 
 class License(object):
-	"""docstring for License"""
+	"""
+	This class is responsible for managing all attributes of lincese class, 
+	like it self ecosystemDataManager, version and index
+	"""
 	def __init__(self, ecosystemDataManager, version, index):
 		super(License, self).__init__()
 		self.ecosystemDataManager = ecosystemDataManager
 		self.version = version
 		self.index = index
+	"""
+	This function is internally called for initialization of the class and set all attributes
+	"""
 
 	def getName(self):
 		versionsHasLicenses = self.ecosystemDataManager.get("VersionsHasLicenses")
 		return versionsHasLicenses[self.version.getIndex()][self.index]
+	"""
+	This function is internally called to return the license name itself 
+	"""
 
 	def getGroup(self):
 		licensesHasGroup = self.ecosystemDataManager.get("LicensesHasGroup")
 		return Group(licensesHasGroup[self.version.getIndex()][self.index])
+	"""
+	This function is internally called to return the license group itself
+	"""
 
 	def setGroup(self, group):
 		licensesHasGroup = self.ecosystemDataManager.get("LicensesHasGroup")
 		licensesHasGroup[self.version.getIndex()][self.index] = group.value
 		return self
-
+	"""
+	This function is internally called to set the new group
+	"""
 	def __hash__(self):
 		return self.index
-
+	"""
+	This overwritten function is internally called to return the self index for hash
+	"""
 	def __eq__(self, other):
 		if type(other) == str:
 			return self.getName() == other
 		elif type(other) != type(self):
 			return False
 		return self.getName() == other.getName()
-
+	"""
+	This overwritten function is internally called to compare this license with other license by license Name
+	"""
 	def __str__(self):
 		return self.getName()
+	"""
+	This overwritten function is internally called to return license Name
+	"""
--- ecosystemDataManager/ocurrence.py
+++ ecosystemDataManager/ocurrence.py
@@ -1,7 +1,10 @@
 from .dependency import Dependency
 
 class Ocurrence(object):
-	"""docstring for Ocurrence"""
+	"""
+	This class is responsible for represent an edge of a graph, and that edge is the 
+	occurrence of a dependency
+	"""
 	def __init__(self, ecosystemDataManager, outVersion, inVersion):
 		super(Ocurrence, self).__init__()
 		if not ecosystemDataManager or not outVersion or not inVersion:
@@ -9,15 +12,27 @@ class Ocurrence(object):
 		self.ecosystemDataManager = ecosystemDataManager
 		self.outVersion = outVersion
 		self.inVersion = inVersion
-
+	"""
+	This function is internally called for initialization of the class and set all attributes.
+	If haven't set all requested this class cound't be initialized.
+	"""
 	def getOutVersion(self):
 		return self.outVersion
-
+	"""
+	This function is internally called to return the vertex that the edge has its origin
+	"""
 	def getInVersion(self):
 		return self.inVersion
-
+	"""
+	This function is internally called to return the vertex that this vertex points
+	"""
 	def getDependency(self):
 		return Dependency(self.ecosystemDataManager, self.inVersion, self.outVersion, None)
-
+	"""
+	This function is internally called to return the inverse of this ocurrence. That is, a dependency edge.
+	"""
 	def __str__(self):
 		return self.outVersion.__str__() + " --> " + self.inVersion.__str__()
+	"""
+	This overwritten function is internally called to return license Name
+	"""
